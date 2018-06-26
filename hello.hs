300f472ef928737efd6f210abb4f261e27d9ae52
Siva Karthikeyan Krishnan <seesiva@gmail.com>
2018-06-26 05:34:17 +0530

    updated with single command line arguments

--- hello.hs
+++ hello.hs
@@ -2,4 +2,7 @@ module Main where
 import System.Environment
 
 main :: IO()
-main = putStrLn ("Hello, World") 
+-- main = putStrLn ("Hello, World")
+main = do 
+    args <- getArgs
+    putStrLn ("Hello, " ++ args !! 0)
